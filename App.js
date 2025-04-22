695187e7e51599e8acf0321265308a78e1e4fa11
akku987 <nizamkamal96@gmail.com>
2025-04-22 17:43:14 +0530

    first java

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div",
+    { id: "parent"},
+    React.createElement("div",{id: "child"}, 
+        [React.createElement("h1",{},"i m java",
+            React.createElement("h2",{},"i m java3"))]));
+
+
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    console.log(parent)
+    root.render(parent); 
+
